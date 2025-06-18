0083a45f89e897a298e9447e23db15e2755b6cf5
mariem-zouaghi <zouaghimariem91@gmail.com>
2025-06-18 16:14:12 +0100

    Create OrderRouts.js

---
+++ back/src/Router/OrderRouts.js
@@ -0,0 +1,25 @@
+const express =require("express");
+const Router = express.Router();
+const cors = require("cors");
+const {orderRegisterCtrl, getOrdersCtrl,deleteOrderCtrl,updateOrderCtrl} = require("./../Controllers/OrderController")
+
+Router
+  .route('/')
+  .post(orderRegisterCtrl)
+
+
+Router
+  .route('/:id')
+  .get(getOrdersCtrl);
+ 
+
+Router
+  .route('/:id')
+  .delete(deleteOrderCtrl);
+
+ Router
+  .route('/:id')
+  .patch(updateOrderCtrl);
+
+
+module.exports = Router;
