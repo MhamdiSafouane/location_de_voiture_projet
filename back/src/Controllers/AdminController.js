097ade64c0fc56ff437a7f8ef3a4ea65a7e3d4cf
mariem-zouaghi <zouaghimariem91@gmail.com>
2025-06-18 11:58:34 +0100

    Update AdminController.js

--- back/src/Controllers/AdminController.js
+++ back/src/Controllers/AdminController.js
@@ -2,3 +2,12 @@ const expressAsyncHandler = require('express-async-handler');
 const User = require('./../model/UserSchema');
 const Admin = require("./../model/AdminSchema")
 const validateMongodbID = require('./../ValidateID');
+const generateToken = require('./../GernerateToken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
+const SECRATE_KEY= process.env.SECRATE_KEY
+const AdminRegisterCtrl = async (req, res) => {
+    try {
+        let { password, email } = req.body;
+        let hashedPassword = await bcrypt.hash(password, 10);
