bdabd417d56ed145327f293b1898aa0a905dd7af
jaouadiella <ellajaouadi5@gmail.com>
2025-06-18 08:27:52 +0100

    Create Payment.js

---
+++ front/src/components/Payment/Payment.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import NavLogout from '../../Booking/NavLogout'
+import './../Style/Payment.css'
+import PaymentCard from './PaymentCard'
+
+function Payment() {
+  return (
+    <div className='box'>
+      <NavLogout/>
+     <PaymentCard/>
+    </div>
+  )
+}
+
+export default Payment
