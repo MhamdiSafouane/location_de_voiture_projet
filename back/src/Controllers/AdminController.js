f39502589963a80c9b9e5f5fb937b81174f2b233
mariem-zouaghi <zouaghimariem91@gmail.com>
2025-06-18 12:24:48 +0100

    Update AdminController.js

--- back/src/Controllers/AdminController.js
+++ back/src/Controllers/AdminController.js
@@ -23,3 +23,25 @@ let user = await Admin.findOne({ email });
         res.status(400).json({ status: "Failed", message: err.message });
     }
 }
+
+const AdminLoginCtrl = expressAsyncHandler(async (req, res) => {
+     try{
+        let user = await Admin.findOne({email:req.body.email})
+        if(user){
+            let matchPass = await bcrypt.compare(req.body.password, user.password)//true
+            if(matchPass){
+                const token = await jwt.sign({_id:user._id}, SECRATE_KEY);
+              res.status(200).send({status:"Successfully login" , token:token , name:user.Name, AdminId:user._id})
+            }else{
+                res.status(401).send({ status:"fail", message:"User Details Not Match"})
+            }
+        }else{
+            res.status(401).send({ status:"fail",message:"User Details Not Match"})
+        }
+        }catch(err){
+        res.status(400).send(err.message);
+    }
+});
+
+
+module.exports = {AdminLoginCtrl,AdminRegisterCtrl};
