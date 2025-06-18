2c65e6ac67cd022ca0e3c6d0869e3e7158ce74fa
Yung-AG <yungag99@gmail.com>
2025-06-18 19:52:08 +0100

    Update UserSchema.js

--- back/src/model/UserSchema.js
+++ back/src/model/UserSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');  // for hashing password
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     Name: {
